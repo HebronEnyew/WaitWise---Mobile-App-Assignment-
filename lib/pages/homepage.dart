29a3c8b15a08df4aca6ea3fe9c1b885ac61a48aa
yeabsiraamare <yeabsiraamaree@gmail.com>
2025-12-13 09:33:14 +0300

    added homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,83 +1,13 @@
-/*import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFFFF5CC),  // light yellow background
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                "assets/queue.png",
-                height: 180,
-              ),
-              SizedBox(height: 24),
-
-              Text(
-                "Welcome to waitwise",
-                style: TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-
-              SizedBox(height: 8),
-
-              Text(
-                "Join the virtual line and track your turn in real time.",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16, color: Colors.black54),
-              ),
-
-              SizedBox(height: 30),
-
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/service");
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.yellow[700],
-                  minimumSize: Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                ),
-                child: Text("Get Started",
-                    style: TextStyle(color: Colors.black, fontSize: 18)),
-              ),
-
-              SizedBox(height: 25),
-
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  "Log in for admin",
-                  style: TextStyle(fontSize: 16, color: Colors.black87),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}*/
-
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFFF5CC),  // light yellow background
+      backgroundColor: Color(0xFFFFF5CC),  
 
       body: Stack(
         children: [
-          // MAIN CONTENT AT CENTER
           Center(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24),
@@ -126,7 +56,6 @@ class HomePage extends StatelessWidget {
             ),
           ),
 
-          // ADMIN BUTTON 
           Positioned(
             left: 20,
             bottom: 25,
